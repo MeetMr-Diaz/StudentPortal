be9ff9ed581c860a15a3e8396eca4c98ecfa5bfd
Max <mfdc37@umkc.edu>
2024-02-26 15:53:33 -0600

    Add project files.

---
+++ StudentPortal.Web/Migrations/20240226205939_second Migration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentPortal.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class secondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Subcribe",
+                table: "Student",
+                newName: "Subscribed");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Subscribed",
+                table: "Student",
+                newName: "Subcribe");
+        }
+    }
+}
